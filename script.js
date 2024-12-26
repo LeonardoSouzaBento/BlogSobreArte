a19ba02c7b1e0bcf653f3dbd7703fb5e7fd1933e
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2024-12-26 08:22:27 -0300

    26/12/2024

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let spans_resumo= [...document.querySelectorAll('#resumo span')]
+spans_resumo.forEach((span)=>{
+    addEventListener("click", (el)=>{
+        const lei_rapida= document.getElementById('botIn')
+        if(el.target==spans_resumo[1]){
+            lei_rapida.style.transition= 'opacity 0.3s ease'
+            lei_rapida.style.opacity='0'
+        } 
+        else{
+            lei_rapida.style.transition= 'opacity 0.3s ease';
+            lei_rapida.style.opacity='100'
+        }
+
+    })
+})
+
+
 //Spans
 var spans=[...document.getElementsByTagName('span')];
 spans = spans.filter(span => {
@@ -15,64 +32,47 @@ let spans_collapse = spans.filter(span => {
     return text === 'collapse_all';
 });
 
-// Pais do spans
-let pais_spans= spans_expand.map(span => span.parentElement); //captura, depois operação
-pais_spans.map((pai, i, arr) => { 
-    if (pai.classList.contains("txt")) {
-        arr.splice(i, 1, pai.parentElement);
-    }
-});
-//estilo animação lenta para pais
+// Pais do spans // Animação lenta para pais
+let pais_spans= spans_expand.map(span => span.parentElement.parentElement);
+
 pais_spans.forEach(pai => {
     pai.style.transition = 'height 0.4s ease';
 });
 
-// trios
-let trios = spans_expand.map((expand, index) => {
+let alturaDoPai = pais_spans.map((pai,i) => {
     return {
-        expand: expand,
-        collapse: spans_collapse[index],
-        pai: pais_spans[index], 
-        altura_pai: window.getComputedStyle(pais_spans[index]).height
+        pai: pai, 
+        altura_pai: window.getComputedStyle(pais_spans[i]).height
     };
 });
-console.log([trios[1].altura_pai]);
 
-//Função de mudança de altura das divs
+//Expandir
+spans_expand.forEach(expand => {
+    expand.addEventListener('click', (event) => {
+        const pai = event.target.parentElement.parentElement;  
+        const alturaReal = pai.scrollHeight;
+        const collapse= event.target.previousElementSibling;
 
-trios.forEach(objeto => {
-    objeto.expand.addEventListener('click', () => {
-        let alturaReal = objeto.pai.scrollHeight;
-        objeto.pai.style.height=`${alturaReal}px`;
-        objeto.expand.style.zIndex = -1;
-        objeto.collapse.style.zIndex = '0';
+        pai.style.height = `${alturaReal}px`;
+        expand.style.zIndex = -1;
+        collapse.style.zIndex = '0';
     });
 });
 
-trios.forEach(objeto => {
-    objeto.collapse.addEventListener('click', () => {
-        objeto.pai.style.height= `${objeto.altura_pai}`;
-        objeto.collapse.style.zIndex = -1;
-        objeto.expand.style.zIndex = '0';
-        console.log(alturaCSS);
+//Recolher
+spans_collapse.forEach(collapse => {
+    collapse.addEventListener('click', (event) => {
+        const pai = event.target.parentElement.parentElement;
+        
+        alturaDoPai.forEach(objeto => {
+            if (objeto.pai === pai) {
+                pai.style.height = objeto.altura_pai;
+            }
+        });
+
+        const expand= event.target.nextElementSibling;
+        collapse.style.zIndex = -1;
+        expand.style.zIndex = '0';
     });
 });
 
-
-/*
-let spans_expand = spans.filter((span) => span.innerHTML == 'expand_all');
-let spans_collapse = spans.filter((span) => span.innerHTML == 'collapse_all');
-*/
-/*
-OPERADOR TERNARIO
-const paisCorrigidos = pai_spans.map(pai => 
-    pai.classList.contains("txt") ? pai.parentElement : pai
-);
-*/
-/*var spans=[];
-for (let i = spans_all.length - 1; i >= 0; i--) {
-    if (spans_all[i].innerHTML !== 'collapse_all' && spans_all[i].innerHTML !== 'expand_all') {
-        spans.splice(i, 0, //elementoAtual);
-    }
-}
-*/
